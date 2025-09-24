3c2afc36a0b56172effc7c4a2f772ba78e1e94ac
Daniel Navarro <daniel.navarro@newtech.consulting>
2025-09-24 23:48:08 +0200

    debugging

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,14 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { authClient } from "../lib/auth-client";
 import Header from "./layouts/Header";
 
 export function ProtectedLayout() {
     const { data, isPending } = authClient.useSession();
-
+console.log(data, isPending);
     if (isPending) return <p>Cargando...</p>;
-    if (!data) return <Navigate to="/login" replace />;
+    console.log(data);
+    
+    if (!data) return <></>
 
     return (
         <div className="bg-neutral-950 text-white min-h-screen">
